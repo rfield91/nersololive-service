e97cba2bc550e189b67e5e05a744a44de304c3fe
Mark Field <51247227+rfield91@users.noreply.github.com>
2023-06-24 13:22:11 -0400

    Added work/run upload; general code cleanup

--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,17 @@
 import * as fs from "fs";
-import * as fsp from "fs/promises";
 import dotenv from "dotenv";
-import ResultsHTMLParser from "./ResultsHTMLParser.js";
-import ClassResultsGenerator from "./ClassResultsGenerator.js";
-import PaxResultsGenerator from "./PaxResultsGenerator.js";
-import ResultsUploader from "./ResultsUploader.js";
-import { exec } from "child_process";
 import { debounce } from "lodash-es";
-import RawResultsGenerator from "./RawResultsGenerator.js";
+import consoleErrorBeep from "./ConsoleErrorBeep.js";
+import liveResultsHandler from "./LiveResultsHandler.js";
+import workRunHandler from "./WorkRunHandler.js";
 dotenv.config();
-function consoleErrorBeep() {
-    exec("1..3 | %{ [console]::beep(1000, 500) }", {
-        shell: "powershell.exe",
-    });
-}
-async function uploadResults(resultsUploader, uploadName, results) {
-    const uploadSuccess = await resultsUploader.upload(uploadName, JSON.stringify({
-        results: results,
-    }));
-    if (!uploadSuccess)
-        consoleErrorBeep();
-}
-async function processFile() {
-    // Read results file
-    const htmlContent = await fsp.readFile(process.env.RESULTS_FILE_LOCATION, {
-        encoding: "utf-8",
-    });
-    // Parse results
-    const resultsParser = new ResultsHTMLParser(htmlContent);
-    const parsedResultData = resultsParser.parse();
-    // Generate class results
-    const classResultsGenerator = new ClassResultsGenerator(parsedResultData);
-    const classResults = classResultsGenerator.transform();
-    // Generate PAX results
-    const paxResultsGenerator = new PaxResultsGenerator(parsedResultData);
-    const paxResults = paxResultsGenerator.get();
-    // Generate RAW results
-    const rawResultsGenerator = new RawResultsGenerator(parsedResultData);
-    const rawResults = rawResultsGenerator.get();
-    // Upload results
-    const resultsUploader = new ResultsUploader({
-        connectionString: process.env.AZURE_STORAGE_CONNECTION_STRING,
-        containerName: process.env.AZURE_STORAGE_CONTAINER_NAME,
-    });
-    uploadResults(resultsUploader, process.env.CLASS_RESULTS_UPLOAD_NAME, classResults);
-    uploadResults(resultsUploader, process.env.PAX_RESULTS_UPLOAD_NAME, paxResults);
-    uploadResults(resultsUploader, process.env.RAW_RESULTS_UPLOAD_NAME, rawResults);
-}
+console.log("Importing Heat Assignments");
+workRunHandler();
+console.log("Done importing Heat Assignemnts");
 console.log("Watcher Starting...");
 fs.watch(process.env.RESULTS_FILE_LOCATION, debounce(async () => {
     try {
-        await processFile();
+        await liveResultsHandler();
     }
     catch (err) {
         consoleErrorBeep();
--- src/index.ts
+++ src/index.ts
@@ -1,89 +1,17 @@
 import * as fs from "fs";
-import * as fsp from "fs/promises";
 import dotenv from "dotenv";
-import ResultsHTMLParser from "./ResultsHTMLParser.js";
-import ClassResultsGenerator from "./ClassResultsGenerator.js";
-import PaxResultsGenerator from "./PaxResultsGenerator.js";
-import ResultsUploader from "./ResultsUploader.js";
-import { ClassResults, RawResult, Result } from "./type.js";
-import { exec } from "child_process";
 import { debounce } from "lodash-es";
-import RawResultsGenerator from "./RawResultsGenerator.js";
+import consoleErrorBeep from "./ConsoleErrorBeep.js";
+import liveResultsHandler from "./LiveResultsHandler.js";
+import workRunHandler from "./WorkRunHandler.js";
 
 dotenv.config();
 
-function consoleErrorBeep() {
-    exec("1..3 | %{ [console]::beep(1000, 500) }", {
-        shell: "powershell.exe",
-    });
-}
+console.log("Importing Heat Assignments");
 
-async function uploadResults(
-    resultsUploader: ResultsUploader,
-    uploadName: string,
-    results: any
-) {
-    const uploadSuccess = await resultsUploader.upload(
-        uploadName,
-        JSON.stringify({
-            results: results,
-        })
-    );
+workRunHandler();
 
-    if (!uploadSuccess) consoleErrorBeep();
-}
-
-async function processFile() {
-    // Read results file
-    const htmlContent = await fsp.readFile(process.env.RESULTS_FILE_LOCATION, {
-        encoding: "utf-8",
-    });
-
-    // Parse results
-    const resultsParser: ResultsHTMLParser = new ResultsHTMLParser(htmlContent);
-    const parsedResultData: Result[] = resultsParser.parse();
-
-    // Generate class results
-    const classResultsGenerator: ClassResultsGenerator =
-        new ClassResultsGenerator(parsedResultData);
-    const classResults: ClassResults = classResultsGenerator.transform();
-
-    // Generate PAX results
-    const paxResultsGenerator: PaxResultsGenerator = new PaxResultsGenerator(
-        parsedResultData
-    );
-    const paxResults: Result[] = paxResultsGenerator.get();
-
-    // Generate RAW results
-    const rawResultsGenerator: RawResultsGenerator = new RawResultsGenerator(
-        parsedResultData
-    );
-    const rawResults: RawResult[] = rawResultsGenerator.get();
-
-    // Upload results
-    const resultsUploader: ResultsUploader = new ResultsUploader({
-        connectionString: process.env.AZURE_STORAGE_CONNECTION_STRING,
-        containerName: process.env.AZURE_STORAGE_CONTAINER_NAME,
-    });
-
-    uploadResults(
-        resultsUploader,
-        process.env.CLASS_RESULTS_UPLOAD_NAME,
-        classResults
-    );
-
-    uploadResults(
-        resultsUploader,
-        process.env.PAX_RESULTS_UPLOAD_NAME,
-        paxResults
-    );
-
-    uploadResults(
-        resultsUploader,
-        process.env.RAW_RESULTS_UPLOAD_NAME,
-        rawResults
-    );
-}
+console.log("Done importing Heat Assignemnts");
 
 console.log("Watcher Starting...");
 
@@ -91,7 +19,7 @@ fs.watch(
     process.env.RESULTS_FILE_LOCATION,
     debounce(async () => {
         try {
-            await processFile();
+            await liveResultsHandler();
         } catch (err) {
             consoleErrorBeep();
 
